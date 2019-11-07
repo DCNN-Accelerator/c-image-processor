dfec875d006d663d5072762450d908cce918f767
pbrodhead <pbrodhead8@gmail.com>
2019-11-07 14:26:48 -0500

    working prototype of reading from USB port, still broken

--- diagnostic_tools/UART/usb_read.c
+++ diagnostic_tools/UART/usb_read.c
@@ -9,35 +9,47 @@
 #include <sys/time.h>
 
 #define PORT "/dev/ttyUSB1"
+#define BAUDRATE B9600 
 
 int main()
 {
     short portfd=-1;
     int n,f,len;
     char buf[256],*s;
-    alarm(2);
+    alarm(2); //error handler
     struct termios oldtio,newtio;
+    printf("BAUDRATE: %X\nCRTSCTS:  %X\n", BAUDRATE, CRTSCTS);
+    printf("CS8:      %X\nCREAD:    %X\nCLOCAL:   %X\n", CS8, CREAD, CLOCAL);
     
 #if defined(O_NDELAY) && defined(F_SETFL)
-    portfd = open(PORT, O_RDWR|O_NDELAY);
+    portfd = open(PORT, O_RDWR|O_NDELAY|O_NOCTTY);
     if (portfd >= 0){
         /* Cancel the O_NDELAY flag. */
-        printf("port openend\n");
+        printf("port opened\n");                        //DEBUG
+        //sleep(10);
         
-        tcgetattr(portfd,&oldtio);  
+        tcgetattr(portfd,&oldtio); 
+        printf("got old attributes\n");                 //DEBUG
 
         // set attributes - these flags may change for your device
-        #define BAUDRATE B9600 
-        memset(&newtio, 0x00, sizeof(newtio));  
-        newtio.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;   
+        memset(&newtio, 0x00, sizeof(newtio)); 
+        printf("past memset\n");                        //DEBUG
+        
+        newtio.c_cflag = BAUDRATE | CRTSCTS | CS8 | CREAD & ~CLOCAL;
         newtio.c_iflag = IGNPAR | ICRNL;          
         newtio.c_oflag = 0;  
+        
+        printf("c_cflag: %04X\n", newtio.c_cflag);        //DEBUG
+        
+        newtio.c_cc[VMIN] = 1;
+        newtio.c_cc[VTIME] = 5;
 
         tcflush(portfd, TCIFLUSH);  
         tcsetattr(portfd,TCSANOW,&newtio); 
 
         //reset attributes
-        tcsetattr(portfd,TCSANOW,&oldtio); 
+        //tcsetattr(portfd,TCSANOW,&oldtio); 
+        printf("set port attributes\n");                //DEBUG
         
         n = fcntl(portfd, F_GETFL, 0);
         (void) fcntl(portfd, F_SETFL, n & ~O_NDELAY);
@@ -47,10 +59,16 @@ int main()
 #endif
     if (portfd >= 0) 
     {
+        printf("time to read\n");                       //DEBUG
+        ssize_t size = read(portfd, buf, 1);
+        
         if (len == 0) len = strlen(s);
+        printf("past len check\n");                     //DEBUG
         for(f = 0; f < len && f <100; f++)
             buf[f] = *s++ | 0x80;
+        printf("past for loop\n");                      //DEBUG
         write(portfd, buf, f);
+        
         printf("Do write\n");
         while(portfd>=0){
             printf("%s\n",buf);
